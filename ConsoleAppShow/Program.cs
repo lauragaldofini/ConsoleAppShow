20501ae7261cc3e99c94485468650472f86d2f42
Usuario <Usuario@CPD02-M09>
2024-04-16 12:26:05 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppShow/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppShow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 100;
+            double total = 0;
+            int full = 0;
+            int half = 0;
+           for (int ingresso = 1; ingresso <= 30; ingresso++)
+            {
+                Console.WriteLine("Pode comprovar ser estudante?: S(sim)/N(não) ");
+                int estudante = int.Parse(Console.ReadLine());
+                if (ingresso == 10 && est == 1)  {
+                    valor = valor / 2;
+                    half ++ ;
+            else if (ingresso <= 20 && estudante == 1) ;
+                }
+                half++;
+                valor = (valor * 1.5 / 2;
+            }
+            else if (ingresso <= 30 && estudante == 1) ;
+            {
+                half++;
+                valor = (valor * 3) / 2;
+            }
+            if (ingresso <= 10 && estudante == 2) ;
+            {
+                full++;
+                valor = valor;
+            }
+            else if (ingresso <= 20 && estudante == 2) ;
+            {
+                full++;
+                valor = (valor * 1.5);
+            }
+            else if (ingresso <= 30 && estudante == 2) ;
+
+   
+
+    }
+}
